cf464b8e1fb390446408501369ab083496c07429
prakash2blr <prakash2blr@gmail.com>
2017-07-23 16:12:16 +0530

    added architect and viewer folders

---
+++ architect/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser=require('body-parser');
+var http= require('http');
+
+var app=express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended":false}));
+
+var fs=require('fs');
+app.use(express.static(__dirname+'/'));
+
+app.listen(8080);
